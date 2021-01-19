c86c9d2b230eb1b5db588e2789bcc3b1324e121a
Rakesh R <rj7022804626@gmail.com>
2021-01-19 16:15:33 +0530

    Income remaining 5

--- src/components/TopPage2.js
+++ src/components/TopPage2.js
@@ -242,6 +242,7 @@ class TopPage2 extends Component {
         if (this.state.loadbal === false) {
             console.log('avl bal ' + this.state.avlBalance + 'income rem ' + this.state.income_remaining)
         }
+
         if (this.state.avlBalance < this.state.income_remaining && this.state.loadbal === false) {
             this.setState({ avlBalance: this.state.income_remaining });
             console.log('avl bal 1 ' + this.state.avlBalance + ' income rem 1 ' + this.state.income_remaining)
